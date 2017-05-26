7db7e620fcebc6fcbd5e7f67b9e5e57c36d27fe2
admin <admin@admins-MacBook-Air.local>
2017-05-26 18:42:53 +0300

    Added new print hello world string

--- SurveyKMA_iOS/ViewController.swift
+++ SurveyKMA_iOS/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world!")
+        print("another Hello World ")
     }
 
     override func didReceiveMemoryWarning() {
